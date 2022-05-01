4da0a1d3b92c3ebacc4a515e9c153956fbed1823
Ghasak <mdotgh@gmail.com>
2022-05-02 01:23:12 +0900

    Adding useful info of C++ and CMake

---
+++ CMAKE_FUNDAMENTALS/CMAKE_INDEPTH/Complite_CPP_Using_CMake_and_Make/build/CMakeFiles/myProject.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for myProject.
+# This may be replaced when dependencies are built.
---
+++ CMAKE_FUNDAMENTALS/CMAKE_INDEPTH/Complite_CPP_Using_CMake_and_Make/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+/*  Hello world Function
+    Our starting point for the Program
+*/
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
