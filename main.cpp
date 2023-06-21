d396189cfb1befdfd7eb7c94d2b6be8c2a15a379
Ghasak <mdotgh@gmail.com>
2023-06-21 21:45:31 +0900

    [minor updates] SDL2 support for CPP

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "./dependencies/include/SDL2/SDL.h"
-//#include "./dependencies/SDL2.framework/Headers/SDL.h"
+// #include "./dependencies/SDL2.framework/Headers/SDL.h"
 /* using namespace std; */
 /* int main() { */
 /*         cout << "Hello World!, SDL2. is installed correctly!" << endl; */
@@ -23,16 +23,18 @@
 //  https://wiki.libsdl.org/SDL_RenderDrawLine     */
 //  SDL2 Library is up and running with MacOSX    */
 /****************************************************/
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO) == 0) {
-        SDL_Window* window = NULL;
-        SDL_Renderer* renderer = NULL;
+    if (SDL_Init(SDL_INIT_VIDEO) == 0)
+    {
+        SDL_Window *window = NULL;
+        SDL_Renderer *renderer = NULL;
 
-        if (SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0) {
+        if (SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0)
+        {
             SDL_bool done = SDL_FALSE;
-
-            while (!done) {
+            while (!done)
+            {
                 SDL_Event event;
 
                 SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
@@ -44,29 +46,25 @@ int main(int argc, char* argv[])
                 SDL_RenderDrawLine(renderer, 340, 240, 320, 200);
                 SDL_RenderPresent(renderer);
 
-                while (SDL_PollEvent(&event)) {
-                    if (event.type == SDL_QUIT) {
+                while (SDL_PollEvent(&event))
+                {
+                    if (event.type == SDL_QUIT)
+                    {
                         done = SDL_TRUE;
                     }
                 }
             }
         }
 
-        if (renderer) {
+        if (renderer)
+        {
             SDL_DestroyRenderer(renderer);
         }
-        if (window) {
+        if (window)
+        {
             SDL_DestroyWindow(window);
         }
     }
     SDL_Quit();
     return 0;
 }
-
-
-
-
-
-
-
-
