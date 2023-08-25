c49ecc0e4984d7ad9f2bb0e6def591fca77c1495
Ghasak <mdotgh@gmail.com>
2023-08-25 14:12:57 +0900

    [basics syntax] updated the basic sentences with the CPP

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,7 @@
 ((c++-mode (helm-make-build-dir . "build/debug/")))
+((c++-mode . ((dap-debug-template-configurations . (
+                                                    ("my_app"
+                                                     :type "lldb"
+                                                     :target "/build/debug/main"
+                                                     )
+                                                    )))))
--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,116 @@
+#include <iomanip>
 #include <iostream>
-/*#include "./dependencies/include/SDL2/SDL.h"*/
-//#include "../dependencies/include/SDL2/SDL.h"
-#include <SDL2/SDL.h>
-// #include "./dependencies/SDL2.framework/Headers/SDL.h"
-/* using namespace std; */
-/* int main() { */
-/*         cout << "Hello World!, SDL2. is installed correctly!" << endl; */
-/*         SDL_Init(SDL_INIT_VIDEO); */
-/*         return 0; */
-/*     } */
+#include <random>
+#include <string>
+#include <vector>
 
-/* int main(){ */
-/*         SDL_Init(SDL_INIT_VIDEO); */
-/*         SDL_Window *window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN); */
-/*         SDL_Renderer *renderer = SDL_CreateRenderer(window,-1 , 0); */
-/*         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); */
-/*         SDL_RenderPresent(renderer); */
-/*         SDL_Delay(8000); */
+// *************************************************
+//               Employee Class
+// *************************************************
+class Employee {
+private:
+  std::string name;
+  int id;
+  double salary;
 
-/* } */
+public:
+  Employee() : name(""), id(0), salary(0.0) {}
 
-/****************************************************
-//  How to  Draw a line in SDL2 :
-//  https://wiki.libsdl.org/SDL_RenderDrawLine     */
-//  SDL2 Library is up and running with MacOSX    */
-/****************************************************/
-int main(int argc, char *argv[])
-{
-    if (SDL_Init(SDL_INIT_VIDEO) == 0)
-    {
-        SDL_Window *window = NULL;
-        SDL_Renderer *renderer = NULL;
+  // Parameterized constructor
+  Employee(const std::string &n, int i, double s) : name(n), id(i), salary(s) {}
 
-        if (SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0)
-        {
-            SDL_bool done = SDL_FALSE;
-            while (!done)
-            {
-                SDL_Event event;
+  // Getter methods
+  std::string getName() const { return name; }
+  int getId() const { return id; }
+  double getSalary() const { return salary; }
 
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-                SDL_RenderClear(renderer);
+  // Setter methods
+  void setName(const std::string &n) { name = n; }
+  void setId(int i) { id = i; }
+  void setSalary(double s) { salary = s; }
 
-                SDL_SetRenderDrawColor(renderer, 100, 200, 255, SDL_ALPHA_OPAQUE);
-                SDL_RenderDrawLine(renderer, 320, 200, 300, 240);
-                SDL_RenderDrawLine(renderer, 300, 240, 340, 240);
-                SDL_RenderDrawLine(renderer, 340, 240, 320, 200);
-                SDL_RenderPresent(renderer);
+  // Display method
+  void display() const {
+    std::cout << "Name: " << name << " ID: " << id << " Salary: $" << salary
+              << "\n";
+  }
+};
 
-                while (SDL_PollEvent(&event))
-                {
-                    if (event.type == SDL_QUIT)
-                    {
-                        done = SDL_TRUE;
-                    }
-                }
-            }
-        }
+// *************************************************
+//             Declare the function
+// *************************************************
+int my_function(int, int); // this is also called the function signature.
+int my_function_pointer(int *, int *);
+int my_function_reference(int &, int &);
+std::string my_function(std::string, std::string);
 
-        if (renderer)
-        {
-            SDL_DestroyRenderer(renderer);
-        }
-        if (window)
-        {
-            SDL_DestroyWindow(window);
-        }
-    }
-    SDL_Quit();
-    return 0;
+int main(int argc, char *argv[]) {
+  std::cout << "Hello World! Wow" << std::endl;
+
+  std::vector<std::string> names = {"Jack", "Michael", "Luke"};
+  // Randomly selecting a value from the vector
+  std::random_device rd;  // Obtain a random number from hardware
+  std::mt19937 gen(rd()); // Seed the generator
+
+  // Generate random ID between 1000 and 9999
+  std::uniform_int_distribution<> idDistrib(1000, 9999);
+
+  // Generate random salary between 50000.00 and 150000.00
+  // Here, we'll first generate between 5000000 and 15000000 (multiplied by 100)
+  // and then divide by 100 to get two decimal places
+  std::uniform_int_distribution<> salaryDistrib(5000000, 15000000);
+
+  int a = 10;
+  int b = 20;
+  for (int i = 0; i < 10; i++) {
+    // calling the function passing by value
+    // std::cout << "value of i -> [" << my_function(a , i) << "]" << std::endl;
+    // calling the function passing by pointer
+    // std::cout << "value of i -> [" << my_function_pointer(&a , &i) << "]" <<
+    // calling the funcntion passing by reference
+    // std::cout << "value of i -> [" << my_function_reference(a, i) << "]" <<
+    // std::endl;
+    std::uniform_int_distribution<> distrib(
+        0, names.size() - 1); // Define the distribution
+
+    int randomIndex = distrib(gen); // Generate random index
+    std::string randomName =
+        names[randomIndex]; // Get the randomly selected name
+    int randomId = idDistrib(gen);
+    double randomSalary = salaryDistrib(gen) / 100.0;
+    Employee emp1(randomName, randomId, randomSalary);
+    emp1.display();
+    std::string output = my_function(emp1.getName(), "Wow");
+    std::cout << output << "\n";
+  }
+  int my_function_definition(int32_t & param_a, int32_t & param_b);
+
+  return 0;
+}
+
+// *************************************************
+//          Definition of the function
+// *************************************************
+// here we define the body of the funciton
+int my_function(int param_a, int param_b) { return param_a + param_b; }
+
+// Passing by pointer
+/**
+ *  Passing by Pointers
+ * Here we are passing pointers to the fucntion instead of
+ * @param values Container whose values are summed.
+ * @return sum of `values`, or 0.0 if `values` is empty.
+ */
+int my_function_pointer(int *param_a, int *param_b) {
+  return 100 * (*(param_a) + *(param_b));
+}
+
+// passing by reference
+int my_function_reference(int &param_a, int &param_b) {
+  return 1000 * (param_a + param_b);
+}
+
+std::string my_function(std::string input_value, std::string value_extension) {
+  std::string extension = " ==> ";
+  return input_value + extension + value_extension;
 }
